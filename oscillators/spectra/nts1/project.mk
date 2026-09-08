359ff6ea089d8725c765623008507cb7fe4afcb0
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-09 06:13:25 +0800

    Add SPECTRA project configuration

---
+++ oscillators/spectra/nts1/project.mk
@@ -0,0 +1,15 @@
+# SPECTRA project customization for Korg's nutekt-digital dummy-osc Makefile
+
+PROJECT = spectra
+
+UCSRC =
+
+UCXXSRC = src/spectra.cpp
+
+UINCDIR = src
+
+UDEFS =
+
+ULIB =
+
+ULIBDIR =
