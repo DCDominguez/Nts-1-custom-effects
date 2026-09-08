f8cbdb2630b3ef423e27087ef5878f5bcd6f73fe
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-09 05:42:53 +0800

    Add PARALLAX project config

---
+++ effects/parallax/nts1/project.mk
@@ -0,0 +1,15 @@
+# PARALLAX project customization for Korg's nutekt-digital dummy-delfx Makefile
+
+PROJECT = parallax
+
+UCSRC =
+
+UCXXSRC = src/parallax.cpp
+
+UINCDIR = src
+
+UDEFS =
+
+ULIB =
+
+ULIBDIR =
