86071892dfa2d4cb3c79b6379673c7a50d53a04e
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-09 05:26:21 +0800

    Add CHORDGHOST NTS-1 project config

---
+++ effects/chordghost/nts1/project.mk
@@ -0,0 +1,15 @@
+# CHORDGHOST project customization for Korg's nutekt-digital dummy-delfx Makefile
+
+PROJECT = chordghost
+
+UCSRC =
+
+UCXXSRC = src/chordghost.cpp
+
+UINCDIR = src
+
+UDEFS =
+
+ULIB =
+
+ULIBDIR =
