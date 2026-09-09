ee4153f939b56ebd5fe428abc2e47c9a0d2302b4
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-09 09:42:03 +0800

    Add CARRIER NTS-1 project config

---
+++ effects/carrier/nts1/project.mk
@@ -0,0 +1,15 @@
+# CARRIER project customization for Korg's nutekt-digital dummy-modfx Makefile
+
+PROJECT = carrier
+
+UCSRC =
+
+UCXXSRC = src/carrier.cpp
+
+UINCDIR = src
+
+UDEFS =
+
+ULIB =
+
+ULIBDIR =
