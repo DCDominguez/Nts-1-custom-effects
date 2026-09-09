23266697121d9f80e112151523be1bf59db37a46
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-09 09:29:41 +0800

    Add DUST project.mk

---
+++ effects/dust/nts1/project.mk
@@ -0,0 +1,15 @@
+# DUST project customization for Korg's nutekt-digital dummy-modfx Makefile
+
+PROJECT = dust
+
+UCSRC =
+
+UCXXSRC = src/dust.cpp
+
+UINCDIR = src
+
+UDEFS =
+
+ULIB =
+
+ULIBDIR =
