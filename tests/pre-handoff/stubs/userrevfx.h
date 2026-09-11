baa62425d2daa77e71a9525e6959232003094804
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-11 18:19:32 +0800

    Add RevFX host stub

---
+++ tests/pre-handoff/stubs/userrevfx.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "common.h"
+
+enum {
+  k_user_revfx_param_time = 0,
+  k_user_revfx_param_depth = 1,
+  k_user_revfx_param_reserved = 2,
+  k_user_revfx_param_shift_depth = 3
+};
