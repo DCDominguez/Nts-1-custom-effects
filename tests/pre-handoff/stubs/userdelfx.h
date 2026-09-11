724c3284088b4da5f4360c6796ced4055129ca4a
DCDominguez <114814232+DCDominguez@users.noreply.github.com>
2026-09-11 18:19:26 +0800

    Add DelFX host stub

---
+++ tests/pre-handoff/stubs/userdelfx.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "common.h"
+
+enum {
+  k_user_delfx_param_time = 0,
+  k_user_delfx_param_depth = 1,
+  k_user_delfx_param_reserved = 2,
+  k_user_delfx_param_shift_depth = 3
+};
